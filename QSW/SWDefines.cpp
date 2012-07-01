e732152c45bb373a57c8b33c7097777aabe512bd
Denis Yemets <pgsilent@gmail.com>
2012-07-01 22:09:35 +0600

    [43] Return deleted enums *oops*

--- QSW/SWDefines.cpp
+++ QSW/SWDefines.cpp
@@ -4,7 +4,7 @@ quint8 Locale = 0;
 
 QString QSW_VERSION("2.1");
 QString CLIENT_VERSION("2.4.3");
-QString QSW_BUILD("42");
+QString QSW_BUILD("43");
 QString CLIENT_BUILD("8606");
 
 QString SpellStruct[] =
