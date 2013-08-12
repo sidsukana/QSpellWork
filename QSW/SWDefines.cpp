2b3ef7df5b9ae69ac2574a0a07f769b55466258d
Denis Yemets <pgsilent@gmail.com>
2013-08-12 16:42:14 +0500

    [21] Rewrited advanced filter. Now uses QtScript(JavaScript) for writing filter query. Use 'spell' for access to spellInfo fields. A '.' dot begins popup autocomplete fields list for simple choise. The query string should be brought to a boolean result. For example: "spell.Id == 10 || ((spell.Attributes & 0x00010000) && spell.SpellName.indexOf("Frost") >= 0)"

--- QSW/SWDefines.cpp
+++ QSW/SWDefines.cpp
@@ -2,9 +2,8 @@
 
 quint8 Locale = 0;
 
-QString QSW_VERSION("4.2");
 QString CLIENT_VERSION("4.3.4");
-QString QSW_BUILD("20");
+QString QSW_BUILD("21");
 QString CLIENT_BUILD("15595");
 
 QString ProcFlagDesc[] =
